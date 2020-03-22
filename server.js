f90caaa07ee2518315b9765b105e74bf09ae930e
Nicholas Roberto <njr444@gmail.com>
2020-03-22 11:46:02 -0700

    added base routes and user schema

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
 
+//connect database
+connectDb();
+
 app.get('/', (req, res) => res.send('API running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5060;
 
