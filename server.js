35bba2e7915d53e2ee899ed84327e5de1bb80be4
Nicholas Roberto <njr444@gmail.com>
2020-04-02 11:30:27 -0700

    fixed server error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 connectDb();
 
 //init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('API running'));
 
